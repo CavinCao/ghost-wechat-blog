f67ab1a66df8d06d6167458c0cbf8f511eb41535
CavinCao <cavincao2005@163.com>
2018-10-28 20:50:33 +0800

    修复验证授权if-else问题

--- resources/miniprogram/app.js
+++ resources/miniprogram/app.js
@@ -1,5 +1,5 @@
 App({
-  onLaunch: function () {
+  onLaunch: function() {
 
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
@@ -26,32 +26,30 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-  checkUserInfo:function(cb){
+  checkUserInfo: function(cb) {
     let that = this
-    if (that.globalData.userInfo)
-    {
+    if (that.globalData.userInfo) {
       typeof cb == "function" && cb(that.globalData.userInfo, true);
+    } else {
+      wx.getSetting({
+        success: function(res) {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.getUserInfo({
+              success: function(res) {
+                that.globalData.userInfo = JSON.parse(res.rawData);
+                typeof cb == "function" && cb(that.globalData.userInfo, true);
+              }
+            })
+          } else {
+            typeof cb == "function" && cb(that.globalData.userInfo, false);
+          }
+        }
+      })
     }
-    wx.getSetting({
-      success: function (res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = JSON.parse(res.rawData);
-              typeof cb == "function" && cb(that.globalData.userInfo, true);
-            }
-          })
-        }
-        else
-        {
-          typeof cb == "function" && cb(that.globalData.userInfo, false);
-        }
-      }
-    })
   },
   globalData: {
-    openid:"",
+    openid: "",
     userInfo: null,
     //默认图片
     defaultImageUrl: 'http://image.bug2048.com/blogdefault.jpeg?',
